c8c5926d6feb205d783ada11aedc775560198765
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-02-10 15:23:10 +0330

    init project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+	return (
+		<div className="container">
+			<div className="note-header">Headre Note</div>
+			<div className="note-app">
+				<div className="note-container">Notes</div>
+			</div>
+		</div>
+	)
+}
+
+export default App
